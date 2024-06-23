5e6550e15030f148ce77e3d1b7b4eba3f0f0ca46
leticiasfujita <leticiasfujita@gmail.com>
2024-06-23 04:15:26 -0300

    feat(agrofind): adjust on version 1

--- frontend/agroFind/src/app/cadastrousuario/cadastrousuario.page.ts
+++ frontend/agroFind/src/app/cadastrousuario/cadastrousuario.page.ts
@@ -4,16 +4,21 @@ import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } 
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { UserResponse } from './usuario-model';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cadastrousuario',
   templateUrl: './cadastrousuario.page.html',
   styleUrls: ['./cadastrousuario.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule, IonicModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule, IonicModule, HttpClientModule]
 })
 export class CadastrousuarioPage implements OnInit {
  
+  userBody!:UserResponse;
+
   formularioUsuario: FormGroup = new FormGroup({
     nomeCompleto: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -21,22 +26,33 @@ export class CadastrousuarioPage implements OnInit {
     endereco: new FormControl('', Validators.required),
     cidade: new FormControl('', Validators.required),
     estado: new FormControl('', Validators.required),
-    contato: new FormControl('', [Validators.required, Validators.pattern(/^\(\d{2}\) \d{5}-\d{4}$/)]),
+    contato: new FormControl('', [Validators.required]),
     userLogin: new FormControl('', Validators.required),
     senha: new FormControl('', Validators.required)
   });
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() { }
 
-  onSubmit() {
-    console.log('onSubmit chamado');
+  onSubmit() {debugger;
     if (this.formularioUsuario.valid) {
       console.log('Formulário válido, enviando dados', this.formularioUsuario.value);
+
+      this.userBody = {
+        name: this.formularioUsuario.get('nomeCompleto')!.value,
+        email: this.formularioUsuario.get('email')!.value,
+        password: this.formularioUsuario.get('senha')!.value,
+      }
+      this.postNewUser(this.userBody).subscribe(response =>{console.log(response)});
       this.router.navigateByUrl('/home');
     } else {
       console.log('Formulário inválido, verifique os campos.');
     }
+  }
+
+  postNewUser(body:UserResponse):Observable<UserResponse>{
+    let headers = new HttpHeaders();
+    return this.http.post<UserResponse>('http://localhost:3000/',body,{headers,})
   }
 }
---
+++ frontend/agroFind/src/app/cadastrousuario/usuario-model.ts
@@ -0,0 +1,10 @@
+export interface UserResponse{
+    name: string,
+    email: string,
+    password: string
+}
+
+export interface AnouncementResponse{
+    name: string,
+    valor: string,
+}
